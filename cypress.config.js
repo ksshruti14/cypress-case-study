b550d440b1f02ab6040c3677110039963dc012dc
ksshruti14 <148330642+ksshruti14@users.noreply.github.com>
2023-10-18 21:54:50 +1100

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,34 @@
+const { defineConfig } = require("cypress");
+
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprocessor,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", preprocessor(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+module.exports = defineConfig({
+  env: {
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSe8uL-dd5Z4Pzh1_yo9nB5vPoU2yaNZiulrTAbic8ZZbU-YqA/viewform?vc=0&c=0&w=1&flr=0",
+  },
+  defaultCommandTimeout: 6000,
+
+  e2e: {
+    setupNodeEvents,
+    //specPattern: 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/examples/BDD/*.feature'
+
+  },
+
+});
+
